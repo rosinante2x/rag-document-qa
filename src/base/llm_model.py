c66635d7c335ea1fce275a686657a2e30a8c9e1b
rosinante2x <tavietduc2019@gmail.com>
2026-03-05 15:19:24 +0700

    sua file llm_model.py

--- src/base/llm_model.py
+++ src/base/llm_model.py
@@ -1,13 +1,19 @@
 import os
+from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-def get_llm():
+load_dotenv()
 
-    os.environ["GOOGLE_API_KEY"] = "AIzaSyBud2k3nOgCCuP4iLfLjiZSswag0pxbElY"
-
-    llm = ChatGoogleGenerativeAI(
-        model="gemini-3.1-flash-lite-preview",
-        temperature=0.1
-    )
-
-    return llm
+class LLM_Model:
+    def __init__(self):
+        api_key = os.getenv("GEMINI_API_KEY")
+        
+        if not api_key:
+            raise ValueError("Không tìm thấy GEMINI_API_KEY. Vui lòng kiểm tra file .env!")
+            
+        self.llm = ChatGoogleGenerativeAI(
+            model="gemini-3.1-flash-lite-preview", # Hoặc model bạn đang dùng
+            google_api_key=api_key,
+            temperature=0.1
+        )
+        
